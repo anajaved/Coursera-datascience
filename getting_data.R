ef892b6de92425af5677096c8cc28d448bb71cf1
Ana Javed <ana.javed.3@gmail.com>
2017-05-02 11:09:51 -0400

    Cleaned up oauth code
    Github API calls in R

--- getting_data.R
+++ getting_data.R
@@ -4,21 +4,9 @@ library(httr)
 require(jsonlite)
 
 # -- Find OAuth settings for github:
+# -- Using API
 
 github <- oauth_endpoints("github")
-
-my_app <- oauth_app("github", key="8eb5b31747577c42c737", 
-                secret="00a11ce07751e1ffc33a9ac532ce6ffc78ad4566")
-
-github_token <- oauth2.0_token(oauth_endpoints("github"), my_app)
-
-#Using API
-req <- GET("https://api.github.com/users/jtleek/repos", config(token = github_token))
-stop_for_status(req)
-output <- content(req)
-list(output[[4]]$name, output[[4]]$created_at)
-
-
 
 my_app <- oauth_app("github", key="8eb5b31747577c42c737", 
                     secret="00a11ce07751e1ffc33a9ac532ce6ffc78ad4566")
@@ -27,6 +15,6 @@ findings <- GET("https://api.github.com/users/jtleek/repos",
 
 json1 <- content(findings)
 json2 <- jsonlite::fromJSON(toJSON(json1))
-json2$created_at[[11]]
 list(json2$name[[11]], json2$created_at[[11]])  #"2013-11-07T13:25:07Z"
 
+##########
