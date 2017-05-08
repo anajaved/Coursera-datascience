20a544bb0dc007027a28b7bc3fec015b96a3fb76
Ana Javed <ana.javed.3@gmail.com>
2017-05-07 20:37:47 -0400

    melting & casting data

--- Misc_practice.R
+++ Misc_practice.R
@@ -7,3 +7,21 @@ relevel(yesnofac, ref="yes")
 round(3.345, digits = 1)
 
 #Reshaping data 
+install.packages("Hmisc")
+library(reshape2)
+library(Hmisc)
+data(mtcars)
+
+mtcars$carname <- rownames(mtcars)
+head(mtcars$carname, 5)
+
+#Melting Data 
+carMelt <- melt(mtcars, id=c("carname", "gear","cyl"), 
+                measure.vars =c("mpg","hp"))
+
+head(carMelt, 3)  #shows mpg variable
+tail(carMelt, 3)  #shows hp variable 
+
+#Casting Data 
+cylData <- dcast(carMelt, cyl~variable, mean)
+cylData  #provides mean mpg & hp for each cyl 
